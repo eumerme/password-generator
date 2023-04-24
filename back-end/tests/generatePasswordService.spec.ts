990dad3c02ab45d7653ed22a496779158b9a64b5
eumerme <euthaisetau@gmail.com>
2023-04-24 09:11:12 -0300

    refactor: api url

--- back-end/tests/generatePasswordService.spec.ts
+++ back-end/tests/generatePasswordService.spec.ts
@@ -1,40 +1,41 @@
 import supertest from "supertest";
 import app from "../src/app";
 
+const baseURL = "/api/generate-password";
+
 describe("POST /generate-password", () => {
-  it("should generate a new password", async () => {
-    const body = { length: 12 };
-    const result = await supertest(app).post("/generate-password").send(body);
+	it("should generate a new password", async () => {
+		const body = { length: 12 };
+		const result = await supertest(app).post(baseURL).send(body);
 
-    expect(result.status).toEqual(201);
-  });
+		expect(result.status).toEqual(201);
+	});
 
-  it("should not generate a new password without the length", async () => {
-    const body = {};
-    const result = await supertest(app).post("/generate-password").send(body);
+	it("should not generate a new password without the length", async () => {
+		const body = {};
+		const result = await supertest(app).post(baseURL).send(body);
 
-    expect(result.status).toEqual(422);
-  });
+		expect(result.status).toEqual(422);
+	});
 
-  it("should not generate a new password if the length is negative", async () => {
-    const body = {length: -1};
-    const result = await supertest(app).post("/generate-password").send(body);
+	it("should not generate a new password if the length is negative", async () => {
+		const body = { length: -1 };
+		const result = await supertest(app).post(baseURL).send(body);
 
-    expect(result.status).toEqual(500);
-  });
+		expect(result.status).toEqual(500);
+	});
 
-  it("should not generate a new password if length is zero", async () => {
-    const body = {length: 0};
-    const result = await supertest(app).post("/generate-password").send(body);
+	it("should not generate a new password if length is zero", async () => {
+		const body = { length: 0 };
+		const result = await supertest(app).post(baseURL).send(body);
 
-    expect(result.status).toEqual(500);
-  });
+		expect(result.status).toEqual(500);
+	});
 
-  it("should not generate a new password if the length is greater than 25", async () => {
-    const body = {length: 26};
-    const result = await supertest(app).post("/generate-password").send(body);
+	it("should not generate a new password if the length is greater than 25", async () => {
+		const body = { length: 26 };
+		const result = await supertest(app).post(baseURL).send(body);
 
-    expect(result.status).toEqual(500);
-  });
-  
+		expect(result.status).toEqual(500);
+	});
 });
